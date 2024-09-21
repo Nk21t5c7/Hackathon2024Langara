dcd54e80659f04b085a3e5492a0d32c43cdd1314
Nodoka Hanaki <nodoka@NodokanoMacBook-Pro.local>
2024-09-21 15:29:41 -0700

    home and css

---
+++ list.js
@@ -0,0 +1,73 @@
+const demoData = [
+    {
+        id: 1,
+        name: 'Apple',
+        breed: 'poodle',
+        location: 'Vancouver',
+        age: 3
+    },
+    {
+        id: 2,
+        name: 'Bear',
+        breed: 'chihuahua',
+        location: 'Richmond',
+        age: 1,
+    },
+    {
+        id: 3,
+        name: 'Cookie',
+        breed: 'shiba',
+        location: 'Surrey',
+        age: 9,
+    },
+    {
+        id: 4,
+        name: 'Dear',
+        breed: 'akita',
+        location: 'Victoria',
+        age: 5,
+    },
+    {
+        id: 5,
+        name: 'Edamame',
+        breed: 'golden retriever',
+        location: 'Vancouver',
+        age: 4,
+    },
+    {
+        id: 6,
+        name: 'Fido',
+        breed: 'beagle',
+        location: 'Burnaby',
+        age: 2
+    },
+    {
+        id: 7,
+        name: 'Ginger',
+        breed: 'bulldog',
+        location: 'Abbotsford',
+        age: 6,
+    },
+    {
+        id: 8,
+        name: 'Holly',
+        breed: 'dachshund',
+        location: 'Langley',
+        age: 3,
+    },
+    {
+        id: 9,
+        name: 'Ivy',
+        breed: 'border collie',
+        location: 'Nanaimo',
+        age: 7,
+    },
+    {
+        id: 10,
+        name: 'Jasper',
+        breed: 'terrier',
+        location: 'Kamloops',
+        age: 5,
+    }
+];
+

21c2d9a7d349ba8862df18178eb736b7a4838f11
Nodoka Hanaki <nodoka@NodokanoMBP.hitronwifi.local>
2024-09-21 22:09:32 -0700

    animal data

--- match.js
+++ match.js
@@ -1,16 +1,15 @@
+import { petData } from "./animal-data.js";
 import { getPets } from "./index.js";
 
-const dogData =[
-    {
-        id:1,
-        name:
-    }
-]
 const showPet = [];
 
+const pets = petData;
 
 async function main(){
-    const pets = await getPets();
+    pets.map(pet => pet.name);
+    const petDataConsole = await getPets();
+    console.log(petDataConsole);
+    
     displayPet(pets);
 }
 
@@ -20,29 +19,29 @@ function displayPet(pets){
     console.log(pets);
     const random = Math.trunc(Math.random()*12);
     let count = 0;
-    // while(count === 1){
-    //     if(!showPet.include(pets[random].id)){
-    //         showPet.push(pets[random].id);
+    while(count === 1){
+        if(!showPet.include(pets[random].id)){
+            showPet.push(pets[random].id);
 
-    //         petName.textContent = pets[random].name;
-    //         age.textContent  = pets[random].age;
-    //         breed.textContent = pets[random].breed;
-    //         // petImg.src = ''
-    //         count ++;
-    //     }
+            petName.textContent = pets[random].name;
+            age.textContent  = pets[random].age;
+            breed.textContent = pets[random].breed;
+            petImg.src = ''
+            count ++;
+        }
 
-    //     console.log(petName.textContent);
+        console.log(petName.textContent);
         
-    // }
+    }
 
-    // if(showPet.length === 12){
-    //     document.querySelector('.petInfo').display = 'none';
-    //     notification.textCotent = 'No data.';
-    //     const backtoHome = document.createElement('button');
-    //     const linktoHome = document.createElement('a');
-    //     backtoHome.textContent = 'Back to Home';
-    //     linktoHome.setAttribute('href', 'home.html')
-    //     notification.appendChild(backtoHome);
-    // }
+    if(showPet.length === 12){
+        document.querySelector('.petInfo').display = 'none';
+        notification.textCotent = 'No data.';
+        const backtoHome = document.createElement('button');
+        const linktoHome = document.createElement('a');
+        backtoHome.textContent = 'Back to Home';
+        linktoHome.setAttribute('href', 'home.html')
+        notification.appendChild(backtoHome);
+    }
 }
 main();
